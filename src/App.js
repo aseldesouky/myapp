0f871aa9305c6d0d943b892ddc5407f3802a0d3e
Ahmed Samir <asamer.work@outlook.com>
2019-07-27 00:55:46 +0300

    change from developer

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 2019
+            Learn React Dev Environment
           </a>
         </header>
       </div>
